26707698a39d3059955bc2106ab2140bdbd34348
Aleksandr Ozhogin <aaozhogin@gmail.com>
2019-09-10 19:46:25 +1000

    add tableView

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Aleksandr Ozhogin on 10/9/19.
+//  Copyright © 2019 Aleksandr Ozhogin. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+   
+    
+    
+}
+
