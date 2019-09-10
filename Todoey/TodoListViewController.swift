642d2f0f29ea7bc265e7cf30dc7f987962679d9f
Aleksandr Ozhogin <aaozhogin@gmail.com>
2019-09-11 09:33:30 +1000

    add newbarbutton, add alert with action

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +24,46 @@ class TodoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+
     }
    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
+    //MARK: Add button
     
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+    var textField = UITextField()
+        
+    let alert = UIAlertController(title: "Add new Todoey", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks add item button in UI Alert
+            if let newitem = textField.text {
+                self.itemArray.append(newitem)
+                self.tableView.reloadData()
+            }
+        }
+        
+    alert.addTextField { (alertTextField) in
+        alertTextField.placeholder = "Create new item"
+        textField = alertTextField
+    }
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+    
+    }
 }
+    
+    
+
 
